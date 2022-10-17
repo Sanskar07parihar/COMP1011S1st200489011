3b06f7a7780f8122f62dbedb42f8f00eb91f56ed
Sanskar Parihar <200489011@student.georgianc.on.ca>
2022-10-17 13:51:02 -0400

    Question 2 c and 3a complete

---
+++ src/main/java/com/example/comp1011s1st200489011/TableViewController.java
@@ -0,0 +1,57 @@
+package com.example.comp1011s1st200489011;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class TableViewController implements Initializable {
+
+
+        @FXML
+        private Button ChangetoGraphView;
+
+        @FXML
+        private TableView<CarSold> carID;
+
+        @FXML
+        private TableColumn<CarSold, Integer> modelYear;
+
+        @FXML
+        private TableColumn<CarSold, String > make;
+
+        @FXML
+        private TableColumn<CarSold, String> model;
+
+        @FXML
+        private TableColumn<CarSold, Double> price;
+
+        @FXML
+        private TableColumn<CarSold, Date> dateSold;
+
+        @FXML
+        void OnChangetoGraphViewClicked(ActionEvent event) throws IOException {
+            SceneManager.Instance().changeScene(event, "population-graphview.fxml");
+        }
+
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            carID.setCellValueFactory(new PropertyValueFactory<>("carId"));
+            modelYear.setCellValueFactory(new PropertyValueFactory<>("modelYear));
+            make.setCellValueFactory(new PropertyValueFactory<>("make"));
+            model.setCellValueFactory(new PropertyValueFactory<>("model));
+            price.setCellValueFactory(new PropertyValueFactory<>("price"));
+            dateSold.setCellValueFactory(new PropertyValueFactory<>("dateSold"));
+
+            tableView.getItems().addAll(DBUtility.getPopulationFromDB());
+        }
+    }
+}
